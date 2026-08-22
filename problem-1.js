2ce927c732b97c75b981e4b76ccbec5a501fc052
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-08-23 01:25:19 +0600

    Added:Problem-1 & 2

---
+++ problem-1.js
@@ -0,0 +1,18 @@
+"use strict";
+const getBatteryStatus = (percentage) => {
+    if (percentage <= 20) {
+        return "Low";
+    }
+    else if (percentage <= 50) {
+        return "Medium";
+    }
+    else if (percentage <= 90) {
+        return "High";
+    }
+    return "Full";
+};
+console.log(getBatteryStatus(10));
+console.log(getBatteryStatus(75));
+console.log(getBatteryStatus(35));
+console.log(getBatteryStatus(110));
+console.log(getBatteryStatus(-10));
